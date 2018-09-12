41e07196fc076ac430dd3af109c12fc516f6ee84
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2018-09-12 15:12:51 +0300

    refactoring

--- README.md
+++ README.md
@@ -2,12 +2,10 @@
 
 [![Build Status](https://travis-ci.org/killmenot/nodemailer-attachments-parser.svg?branch=master)](https://travis-ci.org/killmenot/nodemailer-attachments-parser)
 [![Coverage Status](https://coveralls.io/repos/github/killmenot/nodemailer-attachments-parser/badge.svg?branch=master)](https://coveralls.io/github/killmenot/nodemailer-attachments-parser?branch=master)
-[![Dependency Status](https://gemnasium.com/badges/github.com/killmenot/nodemailer-attachments-parser.svg)](https://gemnasium.com/github.com/killmenot/nodemailer-attachments-parser)
+[![Dependency Status](https://david-dm.org/killmenot/nodemailer-attachments-parser.svg)](https://david-dm.org/killmenot/nodemailer-attachments-parser.svg)
 [![npm version](https://badge.fury.io/js/nodemailer-attachments-parser.svg)](https://badge.fury.io/js/nodemailer-attachments-parser)
 
-Process and parse nodemailer's attachments.
-
-Based on [Andris Reinman](https://github.com/andris9)'s work in [mailcomposer](https://github.com/nodemailer/mailcomposer).
+Process and parse nodemailer's attachments. Based on [Andris Reinman](https://github.com/andris9)'s work in [mailcomposer](https://github.com/nodemailer/mailcomposer).
 
 
 ## Install
@@ -21,9 +19,9 @@ npm install nodemailer-attachments-parser
 ```javascript
 'use strict';
 
-var attachmentsParser = require('nodemailer-attachments-parser');
-var attachments;
-var mail = {
+const attachmentsParser = require('nodemailer-attachments-parser');
+let attachments;
+const mail = {
   attachments = [
     {
       filename: 'text1.txt',
@@ -40,7 +38,7 @@ attachments = attachmentsParser(mail.attachments);
 // { attached: {Array}, related: {Array} }
 
 // using options
-var options = {
+const  options = {
   findRelated: true
 };
 attachments = attachmentsParser(mail.attachments, options);
@@ -54,27 +52,27 @@ attachments = attachmentsParser(mail.attachments, options);
 
 ## Attachments format
 
-It's default nodemailer's format. You can find it here https://github.com/nodemailer/nodemailer#attachments
+It's default nodemailer's format. You can find more details here: https://nodemailer.com/message/attachments/
 
 
 ## Licence
 
-The MIT License (MIT)
+    The MIT License (MIT)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
--- test/attachments-parser.test.js
+++ test/attachments-parser.test.js
@@ -1,58 +1,66 @@
-/* globals context, beforeEach, describe, it */
-/* jshint expr:true */
-
 'use strict';
 
-var expect = require('chai').expect;
-var attachmentsParser = require('../');
-var AttachmentsParser = attachmentsParser.AttachmentsParser;
-var path = require('path');
-var filename = path.join(__dirname, 'fixtures', 'test.txt');
+const path = require('path');
+const expect = require('chai').expect;
+const bufferFrom = require('buffer-from');
+const attachmentsParser = require('../');
+const AttachmentsParser = attachmentsParser.AttachmentsParser;
+const filename = path.join(__dirname, 'fixtures', 'test.txt');
 
-describe('module', function () {
-  var parser;
-  var attachments;
-  var attachment;
+describe('module', () => {
+  let parser;
+  let attachments;
+  let attachment;
 
-  describe('exports', function () {
-    it('attachmentsParser should be a function', function () {
-      expect(attachmentsParser).to.be.a('function');
+  describe('exports', () => {
+    it('attachmentsParser should return empty result', () => {
+      const actual = attachmentsParser();
+
+      console.log(actual);
     });
 
-    it('AttachmentsParser should be a function', function () {
-      expect(AttachmentsParser).to.be.a('function');
+    it('attachmentsParser should return parsed result', () => {
+      const expected = {
+        attached: [],
+        related: []
+      };
+
+      const actual = attachmentsParser(attachments);
+
+      expect(actual).eql(expected);
+    });
+
+    it('AttachmentsParser should be a function', () => {
+      expect(attachmentsParser.AttachmentsParser).be.a('function');
     });
   });
 
-  describe('parse()', function () {
-    beforeEach(function () {
+  describe('parse()', () => {
+    beforeEach(() => {
       parser = new AttachmentsParser();
     });
 
-    it('should be a function', function () {
-      expect(parser.parse).to.be.a('function');
-    });
-
-    it('should be empty', function () {
+    it('should be empty', () => {
       attachments = parser.parse([]);
-      expect(attachments.attached).to.eql([]);
-      expect(attachments.related).to.eql([]);
+
+      expect(attachments.attached).eql([]);
+      expect(attachments.related).eql([]);
     });
 
-    it('filename and content type is derived from path', function () {
+    it('filename and content type is derived from path', () => {
       attachments = [{
         path: filename
       }];
 
       attachment = parser.parse(attachments).attached[0];
 
-      expect(attachment.contentType).to.eql('text/plain');
-      expect(attachment.contentDisposition).to.eql('attachment');
-      expect(attachment.filename).to.be.equal('test.txt');
-      expect(attachment.content.path).to.be.equal(filename);
+      expect(attachment.contentType).eql('text/plain');
+      expect(attachment.contentDisposition).eql('attachment');
+      expect(attachment.filename).be.equal('test.txt');
+      expect(attachment.content.path).be.equal(filename);
     });
 
-    it('file on disk as an attachment', function () {
+    it('file on disk as an attachment', () => {
       attachments = [{
         filename: 'note.txt',
         path: filename
@@ -60,14 +68,14 @@ describe('module', function () {
 
       attachment = parser.parse(attachments).attached[0];
 
-      expect(attachment.contentType).to.eql('text/plain');
-      expect(attachment.contentDisposition).to.eql('attachment');
-      expect(attachment.filename).to.be.equal('note.txt');
-      expect(attachment.content.path).to.be.equal(filename);
+      expect(attachment.contentType).eql('text/plain');
+      expect(attachment.contentDisposition).eql('attachment');
+      expect(attachment.filename).be.equal('note.txt');
+      expect(attachment.content.path).be.equal(filename);
     });
 
-    it('use URL as an attachment', function () {
-      var url = 'https://raw.github.com/killmenot/nodemailer-attachments-parser/master/LICENSE';
+    it('use URL as an attachment (as href)', () => {
+      const url = 'https://raw.github.com/killmenot/nodemailer-attachments-parser/master/LICENSE';
 
       attachments = [{
         filename: 'license.txt',
@@ -76,26 +84,42 @@ describe('module', function () {
 
       attachment = parser.parse(attachments).attached[0];
 
-      expect(attachment.contentType).to.eql('text/plain');
-      expect(attachment.contentDisposition).to.eql('attachment');
-      expect(attachment.filename).to.be.equal('license.txt');
-      expect(attachment.content.href).to.be.equal(url);
+      expect(attachment.contentType).eql('text/plain');
+      expect(attachment.contentDisposition).eql('attachment');
+      expect(attachment.filename).be.equal('license.txt');
+      expect(attachment.content.href).be.equal(url);
     });
 
-    it('data uri as an attachment', function () {
+    it('use URL as an attachment (as path)', () => {
+      const url = 'https://raw.github.com/killmenot/nodemailer-attachments-parser/master/LICENSE';
+
+      attachments = [{
+        filename: 'license.txt',
+        path: url
+      }];
+
+      attachment = parser.parse(attachments).attached[0];
+
+      expect(attachment.contentType).eql('text/plain');
+      expect(attachment.contentDisposition).eql('attachment');
+      expect(attachment.filename).be.equal('license.txt');
+      expect(attachment.content.href).be.equal(url);
+    });
+
+    it('data uri as an attachment', () => {
       attachments = [{
         path: 'data:text/plain;base64,aGVsbG8gd29ybGQ='
       }];
 
       attachment = parser.parse(attachments).attached[0];
 
-      expect(attachment.contentType).to.eql('text/plain');
-      expect(attachment.contentDisposition).to.eql('attachment');
-      expect(attachment.filename).to.be.equal('attachment-1.txt');
-      expect(attachment.content.toString()).to.be.equal('hello world');
+      expect(attachment.contentType).eql('text/plain');
+      expect(attachment.contentDisposition).eql('attachment');
+      expect(attachment.filename).be.equal('attachment-1.txt');
+      expect(attachment.content.toString()).be.equal('hello world');
     });
 
-    it('utf-8 string as an attachment', function () {
+    it('utf-8 string as an attachment', () => {
       attachments = [{
         filename: 'text1.txt',
         content: 'hello world!'
@@ -103,41 +127,41 @@ describe('module', function () {
 
       attachment = parser.parse(attachments).attached[0];
 
-      expect(attachment.contentType).to.eql('text/plain');
-      expect(attachment.contentDisposition).to.eql('attachment');
-      expect(attachment.filename).to.be.equal('text1.txt');
-      expect(attachment.content.toString()).to.be.equal('hello world!');
+      expect(attachment.contentType).eql('text/plain');
+      expect(attachment.contentDisposition).eql('attachment');
+      expect(attachment.filename).be.equal('text1.txt');
+      expect(attachment.content.toString()).be.equal('hello world!');
     });
 
-    it('binary buffer as an attachment', function () {
+    it('binary buffer as an attachment', () => {
       attachments = [{
         filename: 'text2.txt',
-        content: new Buffer('hello world!!', 'utf-8')
+        content: bufferFrom('hello world!!', 'utf-8')
       }];
 
       attachment = parser.parse(attachments).attached[0];
 
-      expect(attachment.contentType).to.eql('text/plain');
-      expect(attachment.contentDisposition).to.eql('attachment');
-      expect(attachment.filename).to.be.equal('text2.txt');
-      expect(attachment.content.toString()).to.be.equal('hello world!!');
+      expect(attachment.contentType).eql('text/plain');
+      expect(attachment.contentDisposition).eql('attachment');
+      expect(attachment.filename).be.equal('text2.txt');
+      expect(attachment.content.toString()).be.equal('hello world!!');
     });
 
-    it('stream as an attachment', function () {
+    it('stream as an attachment', () => {
       attachments = [{
         filename: 'text2.txt',
-        content: new Buffer('hello new world', 'utf-8')
+        content: bufferFrom('hello new world', 'utf-8')
       }];
 
       attachment = parser.parse(attachments).attached[0];
 
-      expect(attachment.contentType).to.eql('text/plain');
-      expect(attachment.contentDisposition).to.eql('attachment');
-      expect(attachment.filename).to.be.equal('text2.txt');
-      expect(attachment.content.toString()).to.be.equal('hello new world');
+      expect(attachment.contentType).eql('text/plain');
+      expect(attachment.contentDisposition).eql('attachment');
+      expect(attachment.filename).be.equal('text2.txt');
+      expect(attachment.content.toString()).be.equal('hello new world');
     });
 
-    it('define custom content type for the attachment', function () {
+    it('define custom content type for the attachment', () => {
       attachments = [{
         filename: 'text.bin',
         content: 'hello world!',
@@ -146,13 +170,13 @@ describe('module', function () {
 
       attachment = parser.parse(attachments).attached[0];
 
-      expect(attachment.contentType).to.eql('text/plain');
-      expect(attachment.contentDisposition).to.eql('attachment');
-      expect(attachment.filename).to.be.equal('text.bin');
-      expect(attachment.content.toString()).to.be.equal('hello world!');
+      expect(attachment.contentType).eql('text/plain');
+      expect(attachment.contentDisposition).eql('attachment');
+      expect(attachment.filename).be.equal('text.bin');
+      expect(attachment.content.toString()).be.equal('hello world!');
     });
 
-    it('encoded string as an attachment', function () {
+    it('encoded string as an attachment', () => {
       attachments = [{
         filename: 'text1.txt',
         content: 'aGVsbG8gd29ybGQh=',
@@ -161,51 +185,51 @@ describe('module', function () {
 
       attachment = parser.parse(attachments).attached[0];
 
-      expect(attachment.contentType).to.eql('text/plain');
-      expect(attachment.contentDisposition).to.eql('attachment');
-      expect(attachment.filename).to.be.equal('text1.txt');
-      expect(attachment.content.toString()).to.be.equal('aGVsbG8gd29ybGQh=');
-      expect(attachment.encoding).to.be.equal('base64');
+      expect(attachment.contentType).eql('text/plain');
+      expect(attachment.contentDisposition).eql('attachment');
+      expect(attachment.filename).be.equal('text1.txt');
+      expect(attachment.content.toString()).be.equal('aGVsbG8gd29ybGQh=');
+      expect(attachment.encoding).be.equal('base64');
     });
 
-    it('define custom content transfer encoding', function () {
+    it('define custom content transfer encoding', () => {
       attachments = [{
         content: 'hello world',
         contentTransferEncoding: '7bit'
       }];
 
       attachment = parser.parse(attachments).attached[0];
-      expect(attachment.contentTransferEncoding).to.be.equal('7bit');
+      expect(attachment.contentTransferEncoding).be.equal('7bit');
     });
 
-    it('define message content type', function () {
+    it('define message content type', () => {
       attachments = [{
         contentType: 'message/partial'
       }];
 
       attachment = parser.parse(attachments).attached[0];
-      expect(attachment.contentDisposition).to.eql('inline');
+      expect(attachment.contentDisposition).eql('inline');
     });
 
-    it('define content id', function () {
+    it('define content id', () => {
       attachments = [{
         cid: 'some-id'
       }];
 
       attachment = parser.parse(attachments).attached[0];
-      expect(attachment.cid).to.eql('some-id');
+      expect(attachment.cid).eql('some-id');
     });
 
-    it('define raw content', function () {
+    it('define raw content', () => {
       attachments = [{
         raw: 'content'
       }];
 
       attachment = parser.parse(attachments).attached[0];
-      expect(attachment.raw).to.eql('content');
+      expect(attachment.raw).eql('content');
     });
 
-    it('define headers', function () {
+    it('define headers', () => {
       attachments = [{
         headers: [{
           key: 'X-Key-Name',
@@ -214,20 +238,20 @@ describe('module', function () {
       }];
 
       attachment = parser.parse(attachments).attached[0];
-      expect(attachment.headers).to.eql([{
+      expect(attachment.headers).eql([{
         key: 'X-Key-Name',
         value: 'val1'
       }]);
     });
 
-    context('When options.findRelated enabled', function () {
-      beforeEach(function () {
+    context('When options.findRelated enabled', () => {
+      beforeEach(() => {
         parser = new AttachmentsParser({
           findRelated: true
         });
       });
 
-      it('define content id', function () {
+      it('define content id', () => {
         attachments = [{
           cid: 'some-id'
         }];
@@ -235,53 +259,58 @@ describe('module', function () {
         attachments = parser.parse(attachments);
         attachment = attachments.related[0];
 
-        expect(attachments.attached).to.eql([]);
-        expect(attachments.related).to.eql([attachment]);
-        expect(attachment.cid).to.eql('some-id');
+        expect(attachments.attached).eql([]);
+        expect(attachments.related).eql([attachment]);
+        expect(attachment.cid).eql('some-id');
 
       });
     });
   });
 
-  describe('_processDataUrl()', function () {
-    beforeEach(function () {
+  describe('_processDataUrl()', () => {
+    beforeEach(() => {
       parser = new AttachmentsParser();
     });
 
-    it('should be a function', function () {
-      expect(parser._processDataUrl).to.be.a('function');
+    it('should do nothing', () => {
+      const element = {};
+
+      parser._processDataUrl(element);
+      expect(element).equal(element);
     });
 
-    it('should do nothing when string is invalid data url', function () {
-      var element = {
+    it('should do nothing when string is invalid data url', () => {
+      const element = {
         href: 'data:text'
       };
 
       parser._processDataUrl(element);
-      expect(element).to.equal(element);
+      expect(element).equal(element);
     });
 
-    it('should process non base64 data', function () {
-      var data = '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><rect fill="#00B1FF" width="100" height="100"/></svg>';
-      var element = {
+    it('should process non base64 data', () => {
+      const data = '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><rect fill="#00B1FF" width="100" height="100"/></svg>';
+      const element = {
         path: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(data)
       };
 
       parser._processDataUrl(element);
-      expect(element.content.toString('utf-8')).to.equal(data);
-      expect(element.contentType).to.equal('image/svg+xml');
-      expect(element.path).to.be.false;
+
+      expect(element.content.toString('utf-8')).equal(data);
+      expect(element.contentType).equal('image/svg+xml');
+      expect(element.path).equal(false);
     });
 
-    it('should process base64 data', function () {
-      var element = {
+    it('should process base64 data', () => {
+      const element = {
         href: 'data:text/plain;base64,SGVsbG8gd29ybGQ='
       };
 
       parser._processDataUrl(element);
-      expect(element.content.toString('ascii')).to.equal('Hello world');
-      expect(element.contentType).to.equal('text/plain');
-      expect(element.href).to.be.false;
+
+      expect(element.content.toString('ascii')).equal('Hello world');
+      expect(element.contentType).equal('text/plain');
+      expect(element.href).equal(false);
     });
   });
 });
